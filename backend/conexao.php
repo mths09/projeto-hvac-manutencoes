5460a5a0f42483d766d1426fa4fd68895ed6a3db
Guilherme Ferreira <guilhermege031187@gmail.com>
2026-08-29 22:33:04 -0300

    Atualiza conexão com banco de dados

--- backend/conexao.php
+++ backend/conexao.php
@@ -1,23 +1,14 @@
 <?php
 
-$servidor = "localhost";
+$host = "localhost";
 $usuario = "root";
 $senha = "";
 $banco = "hvac";
 
-$conexao = new mysqli(
-    $servidor,
-    $usuario,
-    $senha,
-    $banco
-);
+$conexao = new mysqli($host, $usuario, $senha, $banco);
 
 if ($conexao->connect_error) {
-    die("Erro na conexão com o banco de dados: " . $conexao->connect_error);
+    die("Erro na conexão com o banco: " . $conexao->connect_error);
 }
 
-$conexao->set_charset("utf8mb4");
-
-echo "Conexão com o banco realizada com sucesso!";
-
-?>
+$conexao->set_charset("utf8");
